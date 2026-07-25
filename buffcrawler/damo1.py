94c5849d0053d8d4f71bc6ab8e667ab2c3477042
MI1000 <1808305540@qq.com>
2026-07-25 17:38:54 +0800

    完善了代码，下一次更改为搜索页面

--- buffcrawler/damo1.py
+++ buffcrawler/damo1.py
@@ -6,11 +6,13 @@ from DrissionPage.common import By
 
 from cookie_manager import ensure_cookie
 
-
-
+co = ChromiumOptions()
+co.headless(True)  # 无头模式
+co.no_imgs(True)   # 禁用图片
+  
 url= "https://buff.163.com/market/csgo#game=csgo" 
 Settings.set_language('zh_cn')
-bro= Chromium().latest_tab
+bro= Chromium(addr_or_opts=co).latest_tab
 
 def open_web(): 
     bro.set.cookies(manager.cookie)
@@ -18,31 +20,11 @@ def open_web():
     return bro
 
         
-def seek(name):
-    ele= bro.ele('@placeholder=输入物品名称')
-#    if not ele :
-    ele.input(name)
-    bro.ele('@id=search_btn_csgo').click()
-    
-        
 def search():
-    name= bro.ele("tag:li") 
-    print(name.text)
-    
-# data-goods_id="857681" 
-#title="AK-47（StatTrak™） | 墨岩 (略有磨损)"
-# data-goods_id="911195" 
-#title="印花 | Aleksib（闪耀）| 2022年里约热内卢锦标赛"
-    """
-    https://buff.163.com/market/csgo#game=csgo&page_num=2&search=ak-47
-    """
-    
+    name= bro.ele(".cols").text
+    return name
+
 if __name__ == "__main__":
     manager = ensure_cookie()
     open_web()
-    seek("ak-47")
     search()
-#https://buff.163.com/goods/857681饰品网址
-
-# `url_available`
-# 此属性以布尔值返回当前链接是否可用
