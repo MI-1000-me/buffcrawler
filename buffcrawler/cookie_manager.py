6e747afb92e4d1c8274725a06ed6bd7bff7b30cc
MI1000 <1808305540@qq.com>
2026-09-04 14:26:01 +0800

    重新重构饼干罐

--- buffcrawler/cookie_manager.py
+++ buffcrawler/cookie_manager.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from pathlib import Path
 
 from DrissionPage import ChromiumOptions
@@ -6,26 +7,37 @@ from DrissionPage import Chromium
 
 url="https://buff.163.com/market/csgo#game=csgo"
 
+logging.basicConfig(
+                    level=logging.INFO,
+                    format="%(levelname)s - %(asctime)s - %(funcName)s - %(message)s",
+                     datefmt="%Y-%m-%d %H:%M:%S",
+                     filename="cookies.log",
+                     filemode="a",
+                     encoding= "utf-8",
+                     )
 
 co = ChromiumOptions()
 co.headless(False) # 非无头模式
 
-class CookieManager():
+class BuffCookieManager():
     def __init__(self):
         self.bro= Chromium(addr_or_opts=co).latest_tab
         self.url= url
         self.cookie = None
         
     #获取cookie
-    def get_cookie(self):
+    def get_cookie(self) -> list:
         self.bro.get(self.url)
         input("扫码后按回车")
         self.cookie= self.bro.cookies()
+        if not self.cookie:
+            logging.error("cookie未成功读取")
         return self.cookie
 
     #储存cookie
     def store_cookie(self):
         with open("cookie.json","w",encoding="utf8")as cooi:
+            logging.info("正在写入cookie")
             json.dump(self.cookie, cooi)
             
     #校验cookie
@@ -49,23 +61,27 @@ class CookieManager():
 
     #打开cookie
     def open_cookie(self):
-        with open("cookie.json","r",encoding="utf8")as cooi:
-            self.cookie = json.load(cooi)
-        return self.cookie
+        try:
+            with open("cookie.json","r",encoding="utf8") as f:
+                self.cookie = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError):
+            self.cookie = None
 
     #关闭浏览器
     def close(self):
-        self.bro.quit()
+        logging.info("已关闭浏览器")
+        self.browser = Chromium(addr_or_opts=co)
+        self.bro = self.browser.latest_tab
 
 def has_cookie():
     """检查当前目录是否存在 cookie.json"""
     cookie_file = Path("cookie.json")
     return cookie_file.exists()
 
-def ensure_cookie():
+def ensure_cookie()->BuffCookieManager:
     #控制函数
     
-    cookie= CookieManager()
+    cookie= BuffCookieManager()
     
     if not  has_cookie() :
         cookie.get_cookie()
@@ -75,7 +91,10 @@ def ensure_cookie():
         
         if cookie.test_cookie() == False:
             cookie.get_cookie()
-            cookie.store_cookie() 
+            cookie.store_cookie()
+            logging.critical("cookie校验失败")
+    if not cookie.cookie: 
+        logging.error("cookie未成功读取")
     return cookie
         
 
